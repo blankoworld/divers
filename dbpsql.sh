97e7990c672eca7b6c2507f3a7153c880acb2b85
Olivier DOSSMANN <olivier+git@dossmann.net>
2013-04-02 11:51:21 +0200

    [ADD] New script that list psql databases

---
+++ dbpsql.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+#
+# dbpsql.sh
+#
+# Retourne la liste des bases de données de "template1".
+# Explications psql : 
+# -c permet de lancer une commande sous forme de requête dans PSQL
+# -t permet d'enlever l'entête des colonnes et le nombre de lignes résultant
+
+for i in $(psql template1 -c "SELECT datname from pg_database order by datname;" -t)
+do
+  echo $i
+done
