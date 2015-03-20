2ee8e5ce9157e22407989089925edd3cecb0fc0f
Olivier DOSSMANN <olivier+git@dossmann.net>
2015-03-20 11:23:57 +0100

    [ADD] Fix a problem of cdrom ejection for cpv.sh

--- cpv.sh
+++ cpv.sh
@@ -2,7 +2,7 @@
 #
 # Monte un CDROM, en copie le contenu à l'endroit donné et éjecte le CDROM
 
-destination="/home/olivier/Vidéos/"
+destination="/media/olivier/nawak/"
 cdrom_dest="/mnt/cdrom"
 cdrom="/dev/sr0"
 rouge="\e[1;31m"
@@ -18,7 +18,7 @@ fi
 
 mount -t iso9660 -o ro $cdrom $cdrom_dest || echo -e "${jaune}Montage du CDROM échoué${normal}"
 (rsync -avP $cdrom_dest $destination && echo -e "${vert}Copie terminée avec succès${normal}") || (echo -e "${rouge}Échec de la copie${normal}" && exit 1)
-umount $cdrom || (echo -e "${rouge}Échec du démontage${normal}" && exit 1)
+umount "$cdrom" || (echo -e "${rouge}Échec du démontage${normal}" && exit 1)
 eject $cdrom || (echo -e "${rouge}Échec de l'éjection${normal}" && exit 1)
 
 echo "Terminé."
