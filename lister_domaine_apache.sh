555c964e844dcf96b03f8f654111d403aca7fa9f
Olivier DOSSMANN <git@dossmann.net>
2018-03-07 10:22:16 +0100

    [IMP] Nginx/Apache list: permit to define in which directory to look

--- lister_domaine_apache.sh
+++ lister_domaine_apache.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-grep -Eh "Server(Alias|Name)" /etc/apache2/sites-enabled/*|sed -e 's/  / /g' -e 's/^\t//g' -e 's/^ //g'|cut -d " " -f 2|sort -u
+# warning: don't forget '/' at the end
+dir="/etc/apache2/sites-enabled/"
+
+grep -Eh "Server(Alias|Name)" ${dir}*|sed -e 's/  / /g' -e 's/^\t//g' -e 's/^ //g'|cut -d " " -f 2|sort -u
--- lister_domaine_nginx.sh
+++ lister_domaine_nginx.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-grep -rEh "server_name" /etc/nginx/sites-enabled/*| grep -vE "#.*" | sed -e 's/  / /g' |sed -e 's/^[ \t]*//' -e 's/[ \t]*$//' -e 's/  / /g' -e 's/;$//g' -e 's/   / /g'|cut -d ' ' -f 2-5|sort -u
+# warning: don't forget '/' at the end
+dir="/etc/nginx/sites-enabled/"
+
+grep -rEh "server_name" ${dir}*| grep -vE "#.*" | sed -e 's/  / /g' |sed -e 's/^[ \t]*//' -e 's/[ \t]*$//' -e 's/  / /g' -e 's/;$//g' -e 's/   / /g'|cut -d ' ' -f 2-5|sort -u
