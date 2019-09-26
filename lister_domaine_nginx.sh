41e6b75ea0fd86b3b68556f151b13e33365c22b0
Olivier DOSSMANN <git@dossmann.net>
2019-09-26 10:30:34 +0200

    [FIX] Nginx list: improve space deletion
    This also delete some duplicates.

--- lister_domaine_nginx.sh
+++ lister_domaine_nginx.sh
@@ -3,4 +3,4 @@
 # warning: don't forget '/' at the end
 dir="/etc/nginx/sites-enabled/"
 
-grep -rEh "server_name" ${dir}*| grep -vE "#.*" | sed -e 's/  / /g' |sed -e 's/^[ \t]*//' -e 's/[ \t]*$//' -e 's/  / /g' -e 's/;$//g' -e 's/   / /g'|cut -d ' ' -f 2-5|sort -u
+grep -rEh "server_name" ${dir}*| grep -vE "#.*" | sed -e 's/server_name//g' -e 's/^[[:blank:]]*\(.*\);/\1/g'|sort -u
